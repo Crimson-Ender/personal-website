bef582a401ce56b5a134c9e1fbf2d6ca76e6a800
Max Baker <106944706+Crimson-Ender@users.noreply.github.com>
2026-06-16 13:57:31 -0700

    populating data

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Max's Personal Website
 
-placeholder
+This is my personal website, this is where I host my resume, portfolio, and information about myself in lieu of sites like Linkedin that I find more trouble than they are worth.
+
+I programmed this site using React.
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,32 @@
+import { Outlet, useParams } from "react-router";
+import projects from "../data/project.json"
+
 export function Portfolio(){
-        return(
+    const projects = projects
+        
+    
+    return(
         <>
         <h1>Portfolio</h1>
         <h2>This page is a work in progress, please come back later</h2>
+        <Outlet />
         </>
 
+    )
+}
+
+export function ProjectEntry(){
+    const {projectID} = useParams()
+    const project = project
+
+    if(!project){
+        throw new Response("Not Found!", {status:404})
+    }
+
+    return(
+        <>
+        <h1>{project.full_title}</h1>
+        <ul className=""></ul>
+        </>
     )
 }
